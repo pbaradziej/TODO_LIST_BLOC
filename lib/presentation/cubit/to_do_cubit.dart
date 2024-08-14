673744805008b8f6595d6fb47d637bf6cef547c5
Paweł Baradziej <pawel.baradziej0@gmail.com>
2024-08-14 13:57:00 +0200

    Flutter, dart, gradle update

--- lib/presentation/cubit/to_do_cubit.dart
+++ lib/presentation/cubit/to_do_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_list_bloc/data/registers/to_do_register.dart';
 import 'package:todo_list_bloc/domain/entities/to_do_item.dart';
 import 'package:todo_list_bloc/domain/usecases/to_do_actions.dart';
 
@@ -9,54 +8,46 @@ part 'to_do_state.dart';
 
 class ToDoCubit extends Cubit<ToDoState> {
   final ToDoActions toDoActions;
-  final ToDoRegister _toDoRegister;
 
   ToDoCubit({
     ToDoActions? toDoActions,
   })  : toDoActions = toDoActions ?? ToDoActions(),
-        _toDoRegister = ToDoRegister(),
         super(const ToDoState(status: ToDoStatus.initial));
 
   Future<void> initializeToDoItems() async {
     final List<ToDoItem> toDoItems = await toDoActions.getToDoItems();
-    _toDoRegister.initializeToDoItems(toDoItems);
-    _emitToDoState();
+    await _emitToDoState(toDoItems);
   }
 
   Future<void> addToDoItem(ToDoItem item) async {
-    _toDoRegister.addToDoItem(item);
-    await _setToDoItems();
-    _emitToDoState();
+    state.toDoItems.add(item);
+    await _emitToDoState();
   }
 
   Future<void> editToDoItem(String guid, String updatedText) async {
-    _toDoRegister.editToDoItem(guid, updatedText);
-    await _setToDoItems();
-    _emitToDoState();
+    final int toDoItemIndex = state.toDoItems.indexWhere((ToDoItem item) => item.guid == guid);
+    final ToDoItem toDoItem = state.toDoItems.elementAt(toDoItemIndex);
+    state.toDoItems[toDoItemIndex] = toDoItem.copyWith(text: updatedText);
+    await _emitToDoState();
   }
 
   Future<void> removeToDoItem(ToDoItem item) async {
-    _toDoRegister.removeToDoItem(item);
-    await _setToDoItems();
-    _emitToDoState();
+    state.toDoItems.remove(item);
+    await _emitToDoState();
   }
 
   Future<void> changeCompletionOfToDoItem(ToDoItem toDoItem) async {
-    _toDoRegister.changeCompletionOfToDoItem(toDoItem);
-    await _setToDoItems();
-    _emitToDoState();
+    final int toDoItemIndex = state.toDoItems.indexOf(toDoItem);
+    state.toDoItems[toDoItemIndex] = toDoItem.copyWith(isComplete: !toDoItem.isComplete);
+    await _emitToDoState();
   }
 
-  Future<void> _setToDoItems() async {
-    final List<ToDoItem> toDoItems = _toDoRegister.toDoItems;
-    await toDoActions.setToDoItems(toDoItems);
-  }
-
-  void _emitToDoState() {
-    final ToDoState state = ToDoState(
+  Future<void> _emitToDoState([List<ToDoItem>? toDoItems]) async {
+    await toDoActions.setToDoItems(state.toDoItems);
+    final ToDoState toDoState = ToDoState(
       status: ToDoStatus.loaded,
-      toDoItems: _toDoRegister.toDoItems,
+      toDoItems: toDoItems ?? state.toDoItems,
     );
-    emit(state);
+    emit(toDoState);
   }
 }
